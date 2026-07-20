f211623e9fe8a4be61d8747f1ffcfed9e8bc6be5
JavaPyWizard <divyanshkumarsingh29@gmail.com>
2026-07-20 15:03:10 +0530

    Day 23

---
+++ Day 23/D23.c
@@ -0,0 +1,131 @@
+/*
+Problem: Merge Two Sorted Linked Lists - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers (first list)
+- Third line: integer m
+- Fourth line: m space-separated integers (second list)
+
+Output:
+- Print the merged linked list elements, space-separated
+
+Example:
+Input:
+5
+10 20 30 40 50
+4
+15 25 35 45
+
+Output:
+10 15 20 25 30 35 40 45 50
+
+Explanation:
+Compare nodes of both lists, append smaller to result, continue until all nodes are merged.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node 
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node* createNode(int data) 
+{
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertEnd(struct Node *head, int data) 
+{
+    struct Node *newNode = createNode(data);
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node *temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+struct Node* mergeLists(struct Node *head1, struct Node *head2) 
+{
+    struct Node *merged = NULL;
+    struct Node *tail = NULL;
+
+    while (head1 != NULL && head2 != NULL) 
+    {
+        struct Node *temp;
+
+        if (head1->data <= head2->data) 
+        {
+            temp = head1;
+            head1 = head1->next;
+        }
+        else 
+        {
+            temp = head2;
+            head2 = head2->next;
+        }
+
+        temp->next = NULL;
+
+        if (merged == NULL) 
+        {
+            merged = temp;
+            tail = temp;
+        } 
+        else 
+        {
+            tail->next = temp;
+            tail = temp;
+        }
+    }
+
+    if (head1 != NULL)
+        tail->next = head1;
+
+    if (head2 != NULL)
+        tail->next = head2;
+
+    return merged;
+}
+
+void display(struct Node *head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, m, x;
+    struct Node *head1 = NULL;
+    struct Node *head2 = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        head1 = insertEnd(head1, x);
+    }
+
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &x);
+        head2 = insertEnd(head2, x);
+    }
+
+    struct Node *merged = mergeLists(head1, head2);
+
+    display(merged);
+
+    return 0;
+}
